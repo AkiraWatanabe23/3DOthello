51c013e15bf86bc9d2349eadc4fb46d5746ace21
Akira Watanabe <a0323w@gmail.com>
2023-03-19 14:17:23 +0900

    [add] Image,Font

---
+++ Assets/Script/Systems/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStart : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _event = default;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("ゲーム開始");
+            _event?.Invoke();
+        }
+    }
+}
