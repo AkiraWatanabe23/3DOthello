1f6ccf8423dbbec97c59a3f767cff6bc1b19e618
Akira Watanabe <a0323w@gmail.com>
2023-04-23 12:23:17 +0900

    [update] 各点数を比較できるように保存する必要有

--- Assets/Script/CPU/AheadSimurateMove.cs
+++ Assets/Script/CPU/AheadSimurateMove.cs
@@ -8,6 +8,7 @@ public class AheadSimurateMove : SearchBase
     [SerializeField] private int _aheadCount = 1;
 
     private GameManager _manager = default;
+    private int _turn = 0;
 
     public void Start(GameManager manager)
     {
@@ -24,6 +25,8 @@ public class AheadSimurateMove : SearchBase
 
         //探索を終えていなかったら
         //シュミレーションのターンを切り替える
+        //置けるマスを列挙(bool[,])
+
         //相手ターンなら
         //4,シュミレーション
         //5,点数化( <<最小値>> を取得)...マスを保存しておく
@@ -31,12 +34,24 @@ public class AheadSimurateMove : SearchBase
 
         int x = 0;
         int y = 0;
+
         //探索開始時の盤面
         int[,] simurateBoard = (int[,])_manager.Board.Clone();
+        _turn = GameManager.CurrentColor;
 
         while (_aheadCount > 0)
         {
             //探索が終わるまでループ
+            if (_turn == Consts.WHITE)
+            {
+                int maxScore = ScoringMaximize(simurateBoard);
+            }
+            else if (_turn == Consts.BLACK)
+            {
+                int minScore = ScoringMinimize(simurateBoard);
+            }
+
+            _turn *= -1;
             _aheadCount--;
         }
 
@@ -48,17 +63,46 @@ public class AheadSimurateMove : SearchBase
         return board;
     }
 
-    private int ScoringMaximize()
+    /// <summary> シュミレーションした盤面の最大値を返す
+    ///            (自分のターンのシュミレーションに使う) </summary>
+    private int ScoringMaximize(int[,] board)
     {
         int maxScore = int.MinValue;
+        int score = Scoring(board);
 
         return maxScore;
     }
 
-    private int ScoringMinimize()
+    /// <summary> シュミレーションした盤面の最小値を返す
+    ///            (相手のターンのシュミレーションに使う) </summary>
+    private int ScoringMinimize(int[,] board)
     {
         int minScore = int.MaxValue;
+        int score = Scoring(board);
 
         return minScore;
+    }
+
+    /// <summary> 盤面に点数をつける </summary>
+    private int Scoring(int[,] board)
+    {
+        int score = 0;
+
+        for (int i = 1; i < Consts.BOARD_SIZE + 1; i++)
+        {
+            for (int j = 1; j < Consts.BOARD_SIZE + 1; j++)
+            {
+                if (board[i, j] == GameManager.CurrentColor)
+                {
+                    score += board[i, j];
+                }
+                else if (board[i, j] == -GameManager.CurrentColor)
+                {
+                    score -= board[i, j];
+                }
+            }
+        }
+
+        return score;
     }
 }
