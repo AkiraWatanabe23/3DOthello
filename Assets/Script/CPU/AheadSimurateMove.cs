167a552544b8aca5af11add67af9f2db69eacc66
Akira Watanabe <a0323w@gmail.com>
2023-04-25 22:50:44 +0900

    [update] 探索時の値の格納イメージ(このままだと、parentの値が不正確のため、修正する)

--- Assets/Script/CPU/AheadSimurateMove.cs
+++ Assets/Script/CPU/AheadSimurateMove.cs
@@ -1,4 +1,5 @@
 ﻿using Constants;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,8 +12,8 @@ public class AheadSimurateMove : SearchBase
     private GameManager _manager = default;
     /// <summary> ターンを確認する </summary>
     private int _turn = 0;
-    /// <summary> シュミレーションで取得した点数を保存する </summary>
-    private List<int> _scoreList = new();
+    /// <summary> 探索時に、いくつのマスに置くことができるかの変数 </summary>
+    private int _settableCount = 0;
 
     /// <summary> 判定用に使う盤面 </summary>
     private int[,] _simurateBoard = new int[10, 10];
@@ -34,53 +35,72 @@ public class AheadSimurateMove : SearchBase
         //0...x, 1...y
         int[] pos = new int[2];
 
+        //初期化
+        _grids = new List<GridData>[_aheadCount][];
         //探索開始時の情報を保存
         _simurateBoard = (int[,])_manager.Board.Clone();
         _searchDir = (int[,])_manager.MovableDir.Clone();
         _searchPos = (bool[,])movable.Clone();
         _turn = GameManager.CurrentColor;
 
-        while (_aheadCount > 0)
+        for (int i = 0; i < _aheadCount; i++)
         {
-            //探索が終わるまでループ
+            SettableCount();
+            // _grids の各深さに置けるマスの数分インスタンスをつくる
+            _grids[i] = new List<GridData>[_settableCount];
+
+            //指定した深さまで探索
             if (_turn == Consts.WHITE)
             {
-                pos = ScoringMaximize(_simurateBoard);
+                pos = ScoringMaximize(_simurateBoard, i);
             }
             else if (_turn == Consts.BLACK)
             {
-                pos = ScoringMinimize(_simurateBoard);
+                pos = ScoringMinimize(_simurateBoard, i);
             }
 
             _turn *= -1;
-            _aheadCount--;
             SearchMovable();
         }
         int x = pos[0];
         int y = pos[1];
+
+        Array.Clear(_grids, 0, _grids.Length);
 
         return Consts.INPUT_ALPHABET[x - 1].ToString() + Consts.INPUT_NUMBER[y - 1];
     }
 
     /// <summary> シュミレーションした最大値のマスを返す
     ///            (自分のターンのシュミレーションに使う) </summary>
-    private int[] ScoringMaximize(int[,] board)
+    private int[] ScoringMaximize(int[,] board, int depth)
     {
         //探索→点数化(Listに追加)→最大値(最小値)を取得→マスを返す
         int x = 0;
         int y = 0;
-        int score = Scoring(board);
+
+        for (int i = 0; i < _grids[depth].Length; i++)
+        {
+            int score = Scoring(board);
+            //以下仮
+            _grids[depth][i].Add(new GridData(depth - 1, score, x, y));
+        }
 
         return new int[] { x, y };
     }
 
     /// <summary> シュミレーションした最小値のマスを返す
     ///            (相手のターンのシュミレーションに使う) </summary>
-    private int[] ScoringMinimize(int[,] board)
+    private int[] ScoringMinimize(int[,] board, int depth)
     {
         int x = 0;
         int y = 0;
-        int score = Scoring(board);
+
+        for (int i = 0; i < _grids[depth].Length; i++)
+        {
+            int score = Scoring(board);
+            //以下仮
+            _grids[depth][i].Add(new GridData(depth - 1, score, x, y));
+        }
 
         return new int[] { x, y };
     }
@@ -242,6 +262,20 @@ public class AheadSimurateMove : SearchBase
                 if (_searchDir[x, y] != 0)
                 {
                     _searchPos[x, y] = true;
+                }
+            }
+        }
+    }
+
+    private void SettableCount()
+    {
+        for (int x = 1; x < Consts.BOARD_SIZE + 1; x++)
+        {
+            for (int y = 1; y < Consts.BOARD_SIZE + 1; y++)
+            {
+                if (_searchPos[x, y])
+                {
+                    _settableCount++;
                 }
             }
         }
